1689d4ca75217d2da66fd7488f7f9da9fa95de00
Petersongithub <petertiger416@gmail.com>
2016-06-10 15:40:42 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-
+var main = function() {
+  var create = function() {
+    var $PageTop = $(<header>
+      <div class="Navbar">
+        <ul class="NavList">
+          <li class="SmallTab">Videos</li>
+          <li class="SmallTab">Music</li>
+          <li class="SmallTab">Gallery</li>
+          <li class="LargeTab"></li>
+          <li class="SmallTab">News</li>
+          <li class="SmallTab">About</li>
+          <li class="SmallTab">Shop</li>
+        </ul>
+      </div>
+    </header>)
+  };
+};
+$(document).ready(main);
