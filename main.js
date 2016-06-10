7621462cb4ed85791aafd354ac6d31af09b2b61b
Petersongithub <petertiger416@gmail.com>
2016-06-10 15:56:27 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,7 @@
 var main = function() {
-  create($PageTop);
+  var $header = $(<header/>);
+  var $NavBar = $(<div/>).addClass('Navbar').prepend($header);
+  var $NavList = $(<ul/>).addClass('NavList').prepend($NavBar);
+  var $VideosTab = $(<li/>).addClass('SmallTab').append($NavList).text("Videos");
 };
-var create = function() {
-    var $PageTop = $(<header>
-      <div class="Navbar">
-        <ul class="NavList">
-          <li class="SmallTab">Videos</li>
-          <li class="SmallTab">Music</li>
-          <li class="SmallTab">Gallery</li>
-          <li class="LargeTab"></li>
-          <li class="SmallTab">News</li>
-          <li class="SmallTab">About</li>
-          <li class="SmallTab">Shop</li>
-        </ul>
-      </div>
-    </header>);
-  };
 $(document).ready(main);
