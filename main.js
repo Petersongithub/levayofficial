892add9570ed63cabef4c39126b160971009b931
Petersongithub <petertiger416@gmail.com>
2016-06-10 16:19:08 -0600

    Update main.js

--- main.js
+++ main.js
@@ -4,5 +4,6 @@ var main = function() {
   var NavList = $('<ul>').addClass('NavList').prependTo($NavBar);
   var VideosTab = $('<li>').addClass('SmallTab').appendTo($NavList).text('Videos');
   var Content = $('<div>').addClass('content').appendTo('body');
+  var test = $('<li>').addClass('SmallTab').appendTo('.NavList').text("test");
 };
 $(document).ready(main);
